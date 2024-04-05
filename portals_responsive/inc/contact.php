e1e76ec8a7138e6a8254288ff1e2cbc65bb1dc88
P-FMaki <fujita@portals.co.jp>
2024-04-05 13:17:37 +0900

    20240405作業始め

--- portals_responsive/inc/contact.php
+++ portals_responsive/inc/contact.php
@@ -3,19 +3,54 @@
 	<div class="contact">
 		<p class="h_style02">お問い合わせはこちら | ポータルズ整骨院</p>
 		<div class="contact_conts">
-			<p class="contact_tel"><a href="tel:<?php the_author_meta( 'phone', '2' ); ?>"><img src="<?php echo get_template_directory_uri(); ?>/image/all/contact_tel.png" alt=""></a></p>
+			<p class="h_logo"><img src="<?php echo get_template_directory_uri(); ?>/image/all/h_logo_sp.png" alt="六花鍼灸整骨院.小倉駅前店"></p>
 			<div class="contact_uketsuke">
 				<p class="contact_pic"><img src="<?php echo get_template_directory_uri(); ?>/image/all/contact_pic01.jpg" alt=""></p>
-				<div class="contact_info">
-					<p><img src="<?php echo get_template_directory_uri(); ?>/image/all/contact_uketsuke.png" alt=""><span>○曜日は○時まで<br>●曜日、よる●時まで受付</span></p>
-				</div>
+				<dl class="contact_add">
+					<dt>住所：</dt>
+					<dd>〒000-0000<br>○○市○○区○○○町00-00-00　○○○○○ビル　0F</dd>
+					<dt>アクセス：</dt>
+					<dd>○○駅から徒歩○○分</dd>
+				</dl>
 			</div>
-			<dl class="contact_add">
-				<dt>住所：</dt>
-				<dd>〒000-0000<br>○○市○○区○○○町00-00-00　○○○○○ビル　0F</dd>
-				<dt>アクセス：</dt>
-				<dd>○○駅から徒歩○○分</dd>
-			</dl>
+			<p class="contact_tel"><a href="tel:<?php the_author_meta('phone', '2'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/image/all/contact_tel.png" alt=""></a></p>
+				<ul class="contact_inner">
+					<li><a href="<?php the_author_meta('line', '2'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/image/all/contact_line_btn.png" alt="LINE予約"></a></li>
+					<li><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/all/contact_hpb_btn.png" alt="ホットペッパービューティ予約"></a></li>
+				</ul>
+			<div class="contact_info">
+<!--受付時間表-->
+<div class="uketsuke_time">  
+<table>
+  <tbody>
+    <tr>
+      <th>受付時間</th>
+      <td>月</td>
+      <td>火</td>
+      <td>水</td>
+      <td>木</td>
+      <td>金</td>
+      <td>土</td>
+      <td>日</td>
+      <td>祝</td>
+    </tr>
+    <tr>
+      <th>10:00 - 20:00</th>
+      <td>○</td>
+      <td>○</td>
+      <td>○</td>
+      <td>○</td>
+      <td>○</td>
+      <td>○</td>
+      <td>×</td>
+      <td>×</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+<!--ここまで受付時間表-->
+				<p><span>○曜日は○時まで<br>●曜日、よる●時まで受付</span></p>
+			</div>
 			<p class="contact_map"><iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3280.728069257968!2d135.094692!3d34.686811999999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60008695df0e6169%3A0xdaa5e355610c9517!2z5qCq5byP5Lya56S-44Od44O844K_44Or44K6!5e0!3m2!1sja!2sjp!4v1402988469408"></iframe></p>
 		</div>
 
